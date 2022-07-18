b285f6b081963605ec080fff434d89744a5ef10a
Sungho Jang <tjdgh925@gmail.com>
2022-07-18 19:07:38 +0900

    feat: google 로그인 기능 구현

--- src/main/java/com/project/market/api/oauth/service/OAuth2UserServiceFactory.java
+++ src/main/java/com/project/market/api/oauth/service/OAuth2UserServiceFactory.java
@@ -1,5 +1,6 @@
 package com.project.market.api.oauth.service;
 
+import com.project.market.api.oauth.dto.GoogleOAuth2UserInfo;
 import com.project.market.api.oauth.dto.KakaoOAuth2UserInfo;
 import com.project.market.api.oauth.dto.OAuth2UserInfo;
 import com.project.market.domain.member.constant.MemberType;
@@ -12,6 +13,7 @@ public class OAuth2UserServiceFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(MemberType memberType, Map<String, Object> attributes) {
         switch (memberType) {
             case KAKAO: return new KakaoOAuth2UserInfo(attributes);
+            case GOOGLE: return new GoogleOAuth2UserInfo(attributes);
             default: throw new BusinessException(ErrorCode.NOT_VALID_MEMBER_TYPE);
         }
     }
