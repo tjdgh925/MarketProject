e8524a11168300bd3a054124ac3b756dbccb7f32
Sungho Jang <tjdgh925@gmail.com>
2022-07-20 23:20:59 +0900

    feat: 회원 정보 수정 기능 구현

--- src/main/java/com/project/market/web/profile/dto/ProfileUpdateDto.java
+++ src/main/java/com/project/market/web/profile/dto/ProfileUpdateDto.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class ProfileUpdateDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     @Builder
