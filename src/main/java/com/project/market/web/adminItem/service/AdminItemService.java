e96a138ae62bc4865aef9698cfbff186557111ef
Sungho Jang <tjdgh925@gmail.com>
2022-07-23 03:22:23 +0900

    feat: AdminItem Service 구현 #9

---
+++ src/main/java/com/project/market/web/adminItem/service/AdminItemService.java
@@ -0,0 +1,43 @@
+package com.project.market.web.adminItem.service;
+
+import com.project.market.domain.item.entity.Item;
+import com.project.market.domain.item.service.ItemService;
+import com.project.market.domain.itemImage.service.ItemImageService;
+import com.project.market.domain.member.entity.Member;
+import com.project.market.domain.member.service.MemberService;
+import com.project.market.global.error.exception.BusinessException;
+import com.project.market.global.error.exception.ErrorCode;
+import com.project.market.web.adminItem.dto.AdminItemDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AdminItemService {
+
+    private final ItemService itemService;
+    private final ItemImageService itemImageService;
+    private final MemberService memberService;
+
+    @Transactional
+    public Long addNewAdminItem(AdminItemDto adminItemDto, Principal principal) throws IOException {
+        Member member = findMemberByPrincipal(principal);
+        Item item = adminItemDto.toItemEntity(member);
+        itemService.saveItem(item);
+
+        itemImageService.saveItemImages(adminItemDto.getItemImageFiles(), item);
+        return item.getId();
+    }
+
+    private Member findMemberByPrincipal(Principal principal) {
+        String email = principal.getName();
+        Member member = memberService.findByEmail(email)
+                .orElseThrow(() -> new BusinessException(ErrorCode.NO_MATCHING_MEMBER));
+        return member;
+    }
+}
