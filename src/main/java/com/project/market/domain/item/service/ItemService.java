0040c31f223533dff406ee68ab3bbbd82c3e5bdf
Sungho Jang <tjdgh925@gmail.com>
2022-07-22 18:00:13 +0900

    feat: Item Service 구현 #9

---
+++ src/main/java/com/project/market/domain/item/service/ItemService.java
@@ -0,0 +1,19 @@
+package com.project.market.domain.item.service;
+
+import com.project.market.domain.item.entity.Item;
+import com.project.market.domain.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+}
