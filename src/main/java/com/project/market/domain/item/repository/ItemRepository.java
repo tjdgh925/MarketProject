7a9223f526d5b7682f776117d508abed4490f8d8
Sungho Jang <tjdgh925@gmail.com>
2022-07-25 22:49:05 +0900

    feat: Item Repository 상품 조회 기능 구현 #11

--- src/main/java/com/project/market/domain/item/repository/ItemRepository.java
+++ src/main/java/com/project/market/domain/item/repository/ItemRepository.java
@@ -3,5 +3,10 @@ package com.project.market.domain.item.repository;
 import com.project.market.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Override
+    Optional<Item> findById(Long id);
 }
