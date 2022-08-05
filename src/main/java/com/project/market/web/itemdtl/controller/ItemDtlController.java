f0d4d2603a00a4c76d527f1b1abb027cbea88ca6
Sungho Jang <tjdgh925@gmail.com>
2022-08-06 01:10:41 +0900

    feat: ItemDtl Controller 주문 등록 기능 구현 #20

--- src/main/java/com/project/market/web/itemdtl/controller/ItemDtlController.java
+++ src/main/java/com/project/market/web/itemdtl/controller/ItemDtlController.java
@@ -1,13 +1,17 @@
 package com.project.market.web.itemdtl.controller;
 
+import com.project.market.global.error.exception.ErrorCode;
 import com.project.market.web.itemdtl.dto.ItemDtlDto;
+import com.project.market.web.itemdtl.dto.RegisterOrderDto;
 import com.project.market.web.itemdtl.service.ItemDtlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,4 +27,16 @@ public class ItemDtlController {
 
         return "itemdtl/itemdtl";
     }
+
+    @PostMapping("/order")
+    public @ResponseBody ResponseEntity registerOrderItem(@RequestBody RegisterOrderDto registerOrderDto, Principal principal) {
+        try {
+            itemDtlService.registerOrderItem(registerOrderDto, principal);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.valueOf(ErrorCode.NOT_ENOUGH_STOCK.getStatus()));
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
