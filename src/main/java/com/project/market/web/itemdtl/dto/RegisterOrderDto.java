0e0912632767dcfdf214a21f8e38dc500bfdb466
Sungho Jang <tjdgh925@gmail.com>
2022-08-06 01:10:25 +0900

    test: ItemDtl Controller 주문 등록 테스트 코드 작성 #20

--- src/main/java/com/project/market/web/itemdtl/dto/RegisterOrderDto.java
+++ src/main/java/com/project/market/web/itemdtl/dto/RegisterOrderDto.java
@@ -2,13 +2,21 @@ package com.project.market.web.itemdtl.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@Builder
+@NoArgsConstructor
 public class RegisterOrderDto {
 
     private Long itemId;
 
     private int count;
+
+    @Builder
+    public RegisterOrderDto(Long itemId, int count) {
+        this.itemId = itemId;
+        this.count = count;
+    }
+
 }
