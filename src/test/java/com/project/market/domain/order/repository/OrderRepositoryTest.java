35cc3ffcc9e96d68bb9d3d549c7b15de42a18019
Sungho Jang <tjdgh925@gmail.com>
2022-08-05 20:44:51 +0900

    feat: OrderRepository Test 주문 등록 테스트 코드 작성 #20

---
+++ src/test/java/com/project/market/domain/order/repository/OrderRepositoryTest.java
@@ -0,0 +1,84 @@
+package com.project.market.domain.order.repository;
+
+import com.project.market.TestConfig;
+import com.project.market.domain.item.constant.ItemSellStatus;
+import com.project.market.domain.item.entity.Item;
+import com.project.market.domain.item.repository.ItemRepository;
+import com.project.market.domain.member.constant.MemberRole;
+import com.project.market.domain.member.constant.MemberType;
+import com.project.market.domain.member.entity.Member;
+import com.project.market.domain.member.repository.MemberRepository;
+import com.project.market.domain.order.constant.OrderStatus;
+import com.project.market.domain.order.entity.Order;
+import com.project.market.global.config.jpa.AuditConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Import({AuditConfig.class, TestConfig.class})
+class OrderRepositoryTest {
+
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    final Member member = Member.builder()
+            .email("test@email.com")
+            .address("서울특별시")
+            .memberName("tester")
+            .password("password")
+            .memberType(MemberType.EMAIL)
+            .role(MemberRole.USER)
+            .build();
+
+    @BeforeEach
+    public void init() {
+        memberRepository.save(member);
+    }
+
+    @Test
+    public void 주문등록테스트_실패() throws Exception {
+        //given
+        LocalDateTime time = LocalDateTime.now();
+        Order order = Order.builder()
+                .orderStatus(OrderStatus.ORDER)
+                .orderTime(time)
+                .build();
+
+        //when
+        DataIntegrityViolationException result = assertThrows(DataIntegrityViolationException.class, () -> orderRepository.save(order));
+
+        //then
+        assertThat(result).isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    public void 주문등록테스트_성공() throws Exception {
+        LocalDateTime time = LocalDateTime.now();
+        Order order = Order.builder()
+                .orderStatus(OrderStatus.ORDER)
+                .member(member)
+                .orderTime(time)
+                .build();
+
+        //when
+        Order result = orderRepository.save(order);
+
+        //then
+        assertThat(result).isEqualTo(order);
+    }
+
+}
