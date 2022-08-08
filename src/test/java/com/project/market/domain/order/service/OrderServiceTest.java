4d92d83ab46ff36c2c153cc717895af7e09e4911
Sungho Jang <tjdgh925@gmail.com>
2022-08-09 02:55:02 +0900

    test: Order Service 주문 조회 및 수정 테스트 코드 작성 #22

--- src/test/java/com/project/market/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/project/market/domain/order/service/OrderServiceTest.java
@@ -10,21 +10,24 @@ import com.project.market.domain.order.entity.Order;
 import com.project.market.domain.order.entity.OrderItem;
 import com.project.market.domain.order.repository.OrderItemRepository;
 import com.project.market.domain.order.repository.OrderRepository;
+import com.project.market.global.error.exception.BusinessException;
+import com.project.market.global.error.exception.ErrorCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -34,6 +37,9 @@ class OrderServiceTest {
 
     @Mock
     private OrderRepository orderRepository;
+
+    @Mock
+    private OrderItemRepository orderItemRepository;
 
     @Mock
     private MemberRepository memberRepository;
@@ -54,6 +60,12 @@ class OrderServiceTest {
             .price(300)
             .stockNumber(2)
             .member(member)
+            .build();
+
+    OrderItem orderItem =  OrderItem.builder()
+            .orderPrice(300)
+            .count(3)
+            .item(item)
             .build();
 
     @BeforeEach
@@ -94,5 +106,66 @@ class OrderServiceTest {
         verify(orderRepository, times(1)).save(any(Order.class));
     }
 
+    @Test
+    public void 주문조회테스트_싫패() throws Exception {
+        //given
+        doReturn(Optional.empty()).when(orderItemRepository).findById(any(Long.class));
+
+        //when
+        BusinessException result = assertThrows(BusinessException.class, () -> target.findOrderItemById(1L));
+
+        //then
+        assertThat(result.getMessage()).isEqualTo(ErrorCode.NO_MATCHING_ORDER_ITEM.getMessage());
+    }
+
+    @Test
+    public void 주문조회테스트_성공() throws Exception {
+        //given
+        doReturn(Optional.of(orderItem)).when(orderItemRepository).findById(any(Long.class));
+
+        //when
+        OrderItem result = target.findOrderItemById(1L);
+
+        //then
+        assertThat(result).isEqualTo(orderItem);
+    }
+
+    @Test
+    public void 주문삭제테스트_실패() throws Exception {
+        //given
+        doThrow(EmptyResultDataAccessException.class).when(orderItemRepository).deleteById(any(Long.class));
+
+        //when
+        EmptyResultDataAccessException result = assertThrows(EmptyResultDataAccessException.class, () -> target.deleteOrderItemById(2L));
+
+        //then
+        assertThat(result).isInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+    @Test
+    public void 주문삭제테스트_성공() throws Exception {
+        //given
+        doReturn(Optional.empty()).when(orderItemRepository).findById(any(Long.class));
+
+        //when
+        target.deleteOrderItemById(orderItem.getId());
+        BusinessException result = assertThrows(BusinessException.class, () -> target.findOrderItemById(1L));
+
+        //then
+        assertThat(result).isInstanceOf(BusinessException.class);
+    }
+
+    @Test
+    public void 주문개수변경_성공() throws Exception {
+        //given
+        doReturn(Optional.of(orderItem)).when(orderItemRepository).findById(orderItem.getId());
+
+        //when
+        OrderItem result = target.findOrderItemById(orderItem.getId());
+        target.changeOrderItemCount(result, 15);
+
+        //then
+        assertThat(result.getCount()).isEqualTo(15);
+    }
 
 }
