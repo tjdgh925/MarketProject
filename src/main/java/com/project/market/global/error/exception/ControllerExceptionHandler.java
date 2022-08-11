720cdddb43ea28898eb91758c0dfa56001115248
Sungho Jang <tjdgh925@gmail.com>
2022-08-12 01:37:01 +0900

    reafactor: StockException 처리 방법 추가 #20

--- src/main/java/com/project/market/global/error/exception/ControllerExceptionHandler.java
+++ src/main/java/com/project/market/global/error/exception/ControllerExceptionHandler.java
@@ -1,16 +1,27 @@
 package com.project.market.global.error.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Slf4j
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity handleBusinessException(BusinessException exception) {
 
+        log.error("error: ", exception);
         return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(StockException.class)
+    public ResponseEntity handleStockException(StockException exception) {
+
+        log.error("error: ", exception);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
     }
 }
--- src/main/java/com/project/market/global/error/exception/StockException.java
+++ src/main/java/com/project/market/global/error/exception/StockException.java
@@ -1,6 +1,6 @@
 package com.project.market.global.error.exception;
 
-public class StockException extends BusinessException {
+public class StockException extends RuntimeException {
     public StockException(int stockNumber) {
         super(ErrorCode.NOT_ENOUGH_STOCK.getMessage() + "(현재 재고 수량" + stockNumber + ")");
     }
