f61c0da94deb9a9a14368e9d5e4c91c7c222bcc5
Sungho Jang <tjdgh925@gmail.com>
2022-08-12 15:22:26 +0900

    refactor: ControllerExceptionHandler 적용 범위 수정 #20

--- src/main/java/com/project/market/global/error/exception/ControllerExceptionHandler.java
+++ src/main/java/com/project/market/global/error/exception/ControllerExceptionHandler.java
@@ -5,9 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 @Slf4j
 public class ControllerExceptionHandler {
 
