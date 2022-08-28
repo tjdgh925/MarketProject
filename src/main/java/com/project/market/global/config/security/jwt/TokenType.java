f87b3b292483e42aadea123bcfa58b9c4ed6532d
Sungho Jang <tjdgh925@gmail.com>
2022-08-28 21:40:15 +0900

    refactor: Token Type 불필요한 코드 제거 #31

--- src/main/java/com/project/market/global/config/security/jwt/TokenType.java
+++ src/main/java/com/project/market/global/config/security/jwt/TokenType.java
@@ -3,8 +3,4 @@ package com.project.market.global.config.security.jwt;
 public enum TokenType {
 
     ACCESS, REFRESH;
-
-    public static boolean isAccessToken(String tokenType) {
-        return TokenType.ACCESS.name().equals(tokenType);
-    }
 }
