c0b06ecfa12cac1685f9b6d06c88e2f892f9e1aa
Sungho Jang <tjdgh925@gmail.com>
2022-08-30 18:12:41 +0900

    test: FormLogin Controller 회원가입 기능 테스트 코드 작성 #32

---
+++ src/test/java/com/project/market/api/login/form/controller/FormLoginControllerTest.java
@@ -0,0 +1,100 @@
+package com.project.market.api.login.form.controller;
+
+import com.google.gson.Gson;
+import com.project.market.api.login.form.dto.FormRegisterDto;
+import com.project.market.api.login.form.service.FormLoginService;
+import com.project.market.api.login.form.validator.FormRegisterValidator;
+import com.project.market.domain.member.entity.Member;
+import com.project.market.global.error.exception.ControllerExceptionHandler;
+import com.project.market.global.error.exception.InvalidParameterException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class FormLoginControllerTest {
+
+    @InjectMocks
+    private FormLoginController target;
+
+    @Mock
+    private FormLoginService formLoginService;
+
+    @Mock
+    private FormRegisterValidator formRegisterValidator;
+
+    @Mock
+    private Member member;
+
+    private MockMvc mockMvc;
+
+    private Gson gson;
+
+    final String registerUrl = "/api/form/register";
+
+    private final String email = "test@email.com";
+    private final String memberName = "tester";
+    private final String address = "서울특별시";
+    private final String password = "password";
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(target)
+                .setControllerAdvice(ControllerExceptionHandler.class)
+                .build();
+        gson = new Gson();
+    }
+
+    @Test
+    public void 회원가입테스트_실패_유효성검증() throws Exception {
+        //given
+        FormRegisterDto sample = new FormRegisterDto("", memberName, address, password, password);
+        String input = gson.toJson(sample);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post(registerUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(input)).andDo(print());
+
+        //then
+        resultActions
+                .andExpect(status().is4xxClientError())
+                .andExpect(
+                    result -> assertTrue(result.getResolvedException() instanceof InvalidParameterException)
+        );
+    }
+
+    @Test
+    public void 회원가입테스트_성공() throws Exception {
+        //given
+        doReturn(Member.builder().memberName(memberName).build()).when(formLoginService).registerMember(any(FormRegisterDto.class));
+        FormRegisterDto sample = new FormRegisterDto(memberName, email, address, password, password);
+        String input = gson.toJson(sample);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post(registerUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(input)).andDo(print());
+
+        //then
+        resultActions.andExpect(
+                status().isOk()
+        );
+    }
+
+}
