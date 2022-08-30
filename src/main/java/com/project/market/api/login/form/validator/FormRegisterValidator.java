e678b2248f251b40855b0fd9e787e171db6c537a
Sungho Jang <tjdgh925@gmail.com>
2022-08-30 19:02:41 +0900

    refactor: FormRegisterDto 검증 코드 수정 #33

--- src/main/java/com/project/market/api/login/form/validator/FormRegisterValidator.java
+++ src/main/java/com/project/market/api/login/form/validator/FormRegisterValidator.java
@@ -16,6 +16,9 @@ public class FormRegisterValidator implements Validator {
                     "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     private final Pattern pattern;
 
+    private final int MIN_PASSWORD_LENGTH = 7;
+    private final int MAX_PASSWORD_LENGTH = 16;
+
     public FormRegisterValidator() {
         pattern = Pattern.compile(emailRegExp);
     }
@@ -29,26 +32,47 @@ public class FormRegisterValidator implements Validator {
     public void validate(Object target, Errors errors) {
         FormRegisterDto formRequestDto = (FormRegisterDto) target;
 
-        if (formRequestDto.getEmail() == null || formRequestDto.getEmail().trim().isEmpty()) {
-            errors.rejectValue("email","required","이메일은 필수 입력 정보입니다.");
-        } else {
-            Matcher matcher = pattern.matcher(formRequestDto.getEmail());
-            if (!matcher.matches()) {
-                errors.rejectValue("email","bad","올바르지 않은 이메일 형식입니다.");
-            }
-        }
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "memberName","required","이름은 필수 입력 정보입니다.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address","required","주소는 필수 입력 정보입니다.");
+        validateEmail(errors, formRequestDto);
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "memberName", "required", "이름은 필수 입력 정보입니다.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "required", "주소는 필수 입력 정보입니다.");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required", "비밀번호는 필수 입력 정보입니다.");
+        validatePassword(errors, formRequestDto);
+    }
 
-        if (!StringUtils.isEmpty(formRequestDto.getPassword())) {
-            if (formRequestDto.getPassword().length() <= 7 || formRequestDto.getPassword().length() > 16) {
-                errors.rejectValue("password", "bad","비밀번호는 8자 이상, 16자 이하로 입력해주세요.");
-            }
-            if (!formRequestDto.getPassword().equals(formRequestDto.getPasswordConfirm())) {
-                errors.rejectValue("passwordConfirm","noMatch","비밀번호가 일치하지 않습니다.");
-            }
+    private void validatePassword(Errors errors, FormRegisterDto formRequestDto) {
+        if (validatePasswordLength(formRequestDto)) {
+            errors.rejectValue("password", "bad", "비밀번호는 8자 이상, 16자 이하로 입력해주세요.");
+        }
+        if (validatePasswordConfirm(formRequestDto)) {
+            errors.rejectValue("passwordConfirm", "noMatch", "비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    private void validateEmail(Errors errors, FormRegisterDto formRequestDto) {
+        if (isEmailNull(formRequestDto)) {
+            errors.rejectValue("email", "required", "이메일은 필수 입력 정보입니다.");
+            return;
         }
 
+        Matcher matcher = pattern.matcher(formRequestDto.getEmail());
+        if (!matcher.matches()) {
+            errors.rejectValue("email", "bad", "올바르지 않은 이메일 형식입니다.");
+        }
+
+    }
+
+    private boolean isEmailNull(FormRegisterDto formRequestDto) {
+        return formRequestDto.getEmail() == null || formRequestDto.getEmail().trim().isEmpty();
+    }
+
+    private boolean validatePasswordLength(FormRegisterDto formRequestDto) {
+        return formRequestDto.getPassword().length() <= MIN_PASSWORD_LENGTH
+                || formRequestDto.getPassword().length() > MAX_PASSWORD_LENGTH
+                && !StringUtils.isEmpty(formRequestDto.getPassword());
+    }
+
+    private boolean validatePasswordConfirm(FormRegisterDto formRequestDto) {
+        return !formRequestDto.getPassword().equals(formRequestDto.getPasswordConfirm())
+                && !StringUtils.isEmpty(formRequestDto.getPassword());
     }
 }
