42b6e030534465cd97d28eda1260c1b0ad3de60c
Sungho Jang <tjdgh925@gmail.com>
2022-08-31 20:07:25 +0900

    feat: Redis에 Refresh 토큰 저장/조회 기능 구현 #34

--- src/main/java/com/project/market/domain/member/service/MemberService.java
+++ src/main/java/com/project/market/domain/member/service/MemberService.java
@@ -2,18 +2,26 @@ package com.project.market.domain.member.service;
 
 import com.project.market.domain.member.entity.Member;
 import com.project.market.domain.member.repository.MemberRepository;
+import com.project.market.global.config.redis.RedisService;
 import com.project.market.global.error.exception.BusinessException;
+import com.project.market.global.error.exception.EntityNotFoundException;
 import com.project.market.global.error.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final RedisService redisService;
+
+    @Value("${token.refresh-token-expiration-time:1210500000}")
+    private Long refreshTokenExpirationTime;
 
     @Transactional
     public void register(Member member) {
@@ -33,6 +41,12 @@ public class MemberService {
         return memberRepository.findByEmail(email);
     }
 
+    @Transactional(readOnly = true)
+    public Member findMember(String email) {
+        return findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NO_MATCHING_MEMBER));
+    }
+
     @Transactional
     public Member update(String email, String address, String memberName) {
         Member member = findByEmail(email)
@@ -41,5 +55,14 @@ public class MemberService {
         member.updateInfo(address, memberName);
 
         return member;
+    }
+
+    public void saveRefreshToken(Member member, String refreshToken) {
+        redisService.setValues(member.getEmail(), refreshToken, Duration.ofMillis(refreshTokenExpirationTime));
+    }
+
+    public String getRefreshTokenByEmail(String email) {
+        String refreshToken = redisService.getValues(email);
+        return refreshToken;
     }
 }
