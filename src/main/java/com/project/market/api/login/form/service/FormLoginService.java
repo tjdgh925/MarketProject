318bd0c3f2adbd6b4f6db5a4d990d092539082c8
Sungho Jang <tjdgh925@gmail.com>
2022-08-31 21:30:47 +0900

    feat: FormLogin Service 로그인 기능 구현 #34

--- src/main/java/com/project/market/api/login/form/service/FormLoginService.java
+++ src/main/java/com/project/market/api/login/form/service/FormLoginService.java
@@ -1,9 +1,15 @@
 package com.project.market.api.login.form.service;
 
+import com.project.market.api.login.form.dto.FormLoginRequestDto;
 import com.project.market.api.login.form.dto.FormRegisterDto;
 import com.project.market.domain.member.entity.Member;
 import com.project.market.domain.member.service.MemberService;
+import com.project.market.global.config.security.jwt.JwtTokenProvider;
+import com.project.market.global.config.security.jwt.TokenDto;
+import com.project.market.global.error.exception.ErrorCode;
+import com.project.market.global.error.exception.InvalidParameterException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +18,31 @@ import org.springframework.transaction.annotation.Transactional;
 public class FormLoginService {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
 
     @Transactional
     public Member registerMember(FormRegisterDto formRegisterDto) {
         Member newMember = formRegisterDto.toEntity();
         memberService.register(newMember);
         return newMember;
+    }
+
+    public TokenDto formLogin(FormLoginRequestDto formLoginDto){
+        Member member = memberService.findMember(formLoginDto.getEmail());
+        verifyPassword(member, formLoginDto);
+
+        TokenDto tokenDto = tokenProvider.createTokenDto(member);
+        String refreshToken = tokenDto.getRefreshToken();
+        memberService.saveRefreshToken(member, refreshToken);
+
+        return tokenDto;
+    }
+
+    private void verifyPassword(Member member, FormLoginRequestDto formLoginDto) {
+        if(!passwordEncoder.matches(formLoginDto.getPassword(), member.getPassword())){
+            throw new InvalidParameterException(ErrorCode.WRONG_PASSWORD.getMessage());
+        }
     }
 
 }
