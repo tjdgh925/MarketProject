dfdee4802fc2d1b3ae48d21c6b74b739c2121942
Sungho Jang <tjdgh925@gmail.com>
2022-09-07 22:38:18 +0900

    feat: Token Controller 토큰 재발급 기능 구현 #38

---
+++ src/main/java/com/project/market/api/login/token/controller/TokenController.java
@@ -0,0 +1,23 @@
+package com.project.market.api.login.token.controller;
+
+import com.project.market.api.login.token.service.TokenService;
+import com.project.market.global.config.security.jwt.TokenDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class TokenController {
+
+    private final TokenService tokenService;
+
+    @GetMapping("/refreshtoken")
+    public ResponseEntity<TokenDto> refreshToken(@RequestHeader String Authorization, @RequestHeader String refreshToken) {
+        return ResponseEntity.ok(tokenService.refreshToken(Authorization, refreshToken));
+    }
+}
