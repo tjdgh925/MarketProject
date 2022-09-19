29797253fe93373655e1dfb5eb9f968f323eb381
Sungho Jang <tjdgh925@gmail.com>
2022-09-19 19:38:43 +0900

    feat: ProfileApiService  회원정보 조회 기능 구현 #40

--- src/main/java/com/project/market/api/profile/service/ProfileApiService.java
+++ src/main/java/com/project/market/api/profile/service/ProfileApiService.java
@@ -1,5 +1,8 @@
 package com.project.market.api.profile.service;
 
+import com.project.market.api.profile.dto.ProfileResponseDto;
+import com.project.market.domain.member.entity.Member;
+import com.project.market.domain.member.service.MemberService;
 import com.project.market.global.config.security.jwt.JwtTokenProvider;
 import com.project.market.global.error.exception.ErrorCode;
 import com.project.market.global.error.exception.TokenException;
@@ -12,12 +15,26 @@ import org.springframework.stereotype.Service;
 public class ProfileApiService {
 
     private final JwtTokenProvider tokenProvider;
+    private final MemberService memberService;
 
     public String getEmailByToken(String bearerToken) {
         String accessToken = getJwtToken(bearerToken);
 
         return tokenProvider.getEmail(accessToken);
     }
+    public ProfileResponseDto getMemberByToken(String bearerToken) {
+        String accessToken = getJwtToken(bearerToken);
+        Member member = getMember(accessToken);
+
+        return ProfileResponseDto.of(member);
+    }
+
+    private Member getMember(String accessToken) {
+        String email = tokenProvider.getEmail(accessToken);
+        Member member = memberService.findMember(email);
+        return member;
+    }
+
 
     private String getJwtToken(String bearerToken) {
         if (!validateBearerToken(bearerToken)) {
@@ -29,4 +46,5 @@ public class ProfileApiService {
     private boolean validateBearerToken(String bearerToken) {
         return StringUtils.isNotEmpty(bearerToken) && bearerToken.startsWith("Bearer ");
     }
+
 }
